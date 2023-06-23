27dc553d4e0ee12aa474f34e321146f716a05d23
Jerko Sarić <saricjerko86@gmail.com>
2023-06-23 21:38:24 +0200

    added reroute

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -4,12 +4,20 @@ import { AppContext } from "../../context/appContext";
 import RecipesList from "../../components/RecipesList/RecipesList";
 
 import styles from './Favorites.module.scss';
+import { useNavigate } from "react-router-dom";
+import routes from "../../data/routes";
 
 const Favorites = () => {
     const { favorites } = useContext(AppContext);
+    const navigate = useNavigate();
+
+    const reroute = () => {
+        navigate(routes.search);
+    }
 
     return (
         <div className={styles.favorite}>
+            <button onClick={reroute}>back to Search</button>
             <RecipesList recipesData={favorites}/>
         </div>
     )
