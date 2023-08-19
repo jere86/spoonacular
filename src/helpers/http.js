c6f2a1efb1a23606b2418ddec91b602157dd94ea
Jerko Sarić <saricjerko86@gmail.com>
2023-08-19 11:39:18 +0200

    little changes

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
 const SPOONACULAR_API = "https://api.spoonacular.com/";
-const API_KEY = "9bd11a9d9e5a4727b2d9fed3489fd620";
+const API_KEY = "1f97c3a35c4b4fffa5d165a9658983be";
 
 // e1f556e241884f5e8493a5f17cb629bd
 // 9bd11a9d9e5a4727b2d9fed3489fd620
 // beefcc8fafcb437d9863fe9746cb75b2
+// 1f97c3a35c4b4fffa5d165a9658983be
 
 const constructQueryString = (params) => {
   let queryString = "";
@@ -57,9 +58,9 @@ export const postRequest = async (url, data) => {
 };
 
 export const addMealRequest = (url, data) => {
-  axios.post(`${SPOONACULAR_API}${url}&apiKey=${API_KEY}`, data);
+  return axios.post(`${SPOONACULAR_API}${url}&apiKey=${API_KEY}`, data);
 };
 
 export const deleteMealRequest = (url) => {
-  axios.delete(`${SPOONACULAR_API}${url}&apiKey=${API_KEY}`);
+  return axios.delete(`${SPOONACULAR_API}${url}&apiKey=${API_KEY}`);
 };
