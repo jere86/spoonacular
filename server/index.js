45af08e18e627d99479698edf95b0831b88d9f34
Jerko Sarić <saricjerko86@gmail.com>
2024-03-09 16:19:32 +0100

    fix

--- server/index.js
+++ server/index.js
@@ -9,10 +9,16 @@ const app = express();
 
 // Middleware
 app.use(bodyParser.json({ limit: "100mb" }));
-app.use(cors());
-
+app.use(
+  cors({
+    origin: "*",
+    methods: ["POST", "GET", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
+// Database connection
 mongoose.connect(
   "mongodb+srv://saricjerko86:sp4lYkDht1HJ6CZ1@cluster0.cqft3jc.mongodb.net/Spoonacular",
   { useNewUrlParser: true, useUnifiedTopology: true }
@@ -21,26 +27,7 @@ const db = mongoose.connection;
 db.on("error", (error) => console.error("MongoDB connection error:", error));
 db.once("open", () => console.log("Connected to MongoDB"));
 
-app.get("/users", (req, res, next) => {
-  res.set(
-    "Access-Control-Allow-Origin",
-    "https://spoonacular-client.vercel.app"
-  );
-  res.set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-  res.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-
-app.get("/images", (req, res, next) => {
-  res.set(
-    "Access-Control-Allow-Origin",
-    "https://spoonacular-client.vercel.app"
-  );
-  res.set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-  res.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-
+// Routes
 app.get("/", (req, res) => {
   res.json("Hello");
 });
@@ -48,5 +35,6 @@ app.get("/", (req, res) => {
 app.use("/users", usersRouter);
 app.use("/images", imagesRouter);
 
+// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started listening on port ${PORT}`));
