41752efe313a7f2b6fb2d92956e3a22e262a8513
Jerko Sarić <saricjerko86@gmail.com>
2024-03-09 19:38:31 +0100

    fix

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
-// const usersRouter = require("./routes/users");
-// const imagesRouter = require("./routes/images");
+const usersRouter = require("./routes/users");
+const imagesRouter = require("./routes/images");
 
 const app = express();
 
@@ -32,90 +32,8 @@ app.get("/", (req, res) => {
   res.json("Hello");
 });
 
-app.get("/users", async (req, res) => {
-  try {
-    const users = await User.find();
-    res.json(users);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-app.get("/users/:id", getUser, (req, res) => {
-  res.json(res.user);
-});
-
-app.post("/users", async (req, res) => {
-  const user = new User({
-    username: req.body.username,
-    email: req.body.email,
-    userData: req.body.userData,
-    favorites: req.body.favorites,
-    shopingLists: req.body.shopingLists,
-    images: req.body.images,
-  });
-
-  try {
-    const newUser = await user.save();
-    res.status(201).json(newUser);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
-
-app.patch("/users/:id", getUser, async (req, res) => {
-  if (req.body.username != null) {
-    res.user.username = req.body.username;
-  }
-  if (req.body.email != null) {
-    res.user.email = req.body.email;
-  }
-  if (req.body.userData != null) {
-    res.user.userData = req.body.userData;
-  }
-  if (req.body.favorites != null) {
-    res.user.favorites = req.body.favorites;
-  }
-  if (req.body.shopingLists != null) {
-    res.user.shopingLists = req.body.shopingLists;
-  }
-  if (req.body.images != null) {
-    res.user.images = req.body.images;
-  }
-  try {
-    const updatedUser = await res.user.save();
-    res.json(updatedUser);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
-
-app.delete("/users/:id", getUser, async (req, res) => {
-  try {
-    await res.user.deleteOne();
-    res.json({ message: "Deleted User" });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-async function getUser(req, res, next) {
-  let user;
-  try {
-    user = await User.findById(req.params.id);
-    if (user == null) {
-      return res.status(404).json({ message: "Cannot find user!" });
-    }
-  } catch (err) {
-    return res.status(505).json({ message: err.message });
-  }
-
-  res.user = user;
-  next();
-}
-
-// app.use("/users", usersRouter);
-// app.use("/images", imagesRouter);
+app.use("/users", usersRouter);
+app.use("/images", imagesRouter);
 
 // Start server
 const PORT = process.env.PORT || 5000;
