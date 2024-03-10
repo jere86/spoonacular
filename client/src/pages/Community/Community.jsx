9aa9113e785c885859cf56572d5f80172daf5e9f
Jerko Sarić <saricjerko86@gmail.com>
2024-03-11 00:32:14 +0100

    fix

--- client/src/pages/Community/Community.jsx
+++ client/src/pages/Community/Community.jsx
@@ -8,8 +8,6 @@ import axios from "axios";
 const Community = () => {
   const [images, setImages] = useState([]);
 
-  axios.defaults.withCredentials = true;
-
   const getImages = async () => {
     try {
       const response = await axios.get(
