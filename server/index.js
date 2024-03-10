4b2915e2209121b7e7faf7a99c3c2d9b0e88e403
Jerko Sarić <saricjerko86@gmail.com>
2024-03-10 15:38:23 +0100

    fix

--- server/index.js
+++ server/index.js
@@ -7,32 +7,57 @@ const imagesRouter = require("./routes/images");
 const app = express();
 
 // Middleware
-app.use(express.json({ limit: "100mb" }));
 app.use(
-  cors({
-    origin: "https://spoonacular-client.vercel.app",
-    methods: ["POST", "GET", "PATCH", "DELETE"],
-    credentials: true,
+  express.json({
+    limit: "100mb",
   })
 );
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, PUT, PATCH, DELETE"
-//   );
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-//   next();
-// });
+
+const whitelist = ["https://spoonacular-client.vercel.app"];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["POST", "GET", "PATCH", "DELETE"],
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    corsOptions.origin(req.header("Origin"))
+  );
+});
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 // Database connection
-mongoose.connect(
-  "mongodb+srv://saricjerko86:sp4lYkDht1HJ6CZ1@cluster0.cqft3jc.mongodb.net/Spoonacular",
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
+const uri =
+  "mongodb+srv://saricjerko86:sp4lYkDht1HJ6CZ1@cluster0.cqft3jc.mongodb.net/Spoonacular?retryWrites=true&w=majority";
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  });
+
 const db = mongoose.connection;
 db.on("error", (error) => console.error("MongoDB connection error:", error));
-db.once("open", () => console.log("Connected to MongoDB"));
 
 // Routes
 app.get("/", (req, res) => {
@@ -42,6 +67,19 @@ app.get("/", (req, res) => {
 app.use("/users", usersRouter);
 app.use("/images", imagesRouter);
 
+// Middleware for error handling
+app.use((error, req, res, next) => {
+  console.error(error.stack);
+  res.status(500).send("Something went wrong.");
+});
+
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started listening on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server started listening on port ${PORT}`);
+});
+
+// Close the MongoDB connection on process exit
+process.on("exit", () => {
+  db.close();
+});
