1bc62d1773d6a48445cc6a195fa1d606c0d26e54
Jerko Sarić <saricjerko86@gmail.com>
2024-03-10 16:20:59 +0100

    fix

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const usersRouter = require("./routes/users");
 const imagesRouter = require("./routes/images");
 
